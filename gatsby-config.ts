e8788a039af214810e3bf0ca5511302a0f85009a
Dario Voll <dariovoll@gmail.com>
2022-05-07 23:36:21 +0200

    Change dir structure

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,37 +1,38 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Management von kollaborativen Open Source Software Entwicklungs Projekten`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [
-    "gatsby-plugin-netlify-cms", 
-    "gatsby-plugin-mdx", {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "markdown-pages",
-        "path": "./src/markdown-pages/"
-      },
+    siteMetadata: {
+        title: `Management von kollaborativen Open Source Software Entwicklungs Projekten`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: '@chakra-ui/gatsby-plugin',
-      options: {
-        /**
-         * @property {boolean} [resetCSS=true]
-         * if false, this plugin will not use `<CSSReset />
-         */
-        resetCSS: true,
-        /**
-         * @property {boolean} [isUsingColorMode=true]
-         * if false, this plugin will not use <ColorModeProvider />
-         */
-        isUsingColorMode: true,
-      },
-    },
-  ]
+    plugins: [
+        'gatsby-plugin-netlify-cms',
+        'gatsby-plugin-mdx',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'content-pages',
+                path: './src/content-pages/',
+            },
+        },
+        `gatsby-transformer-remark`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: '@chakra-ui/gatsby-plugin',
+            options: {
+                /**
+                 * @property {boolean} [resetCSS=true]
+                 * if false, this plugin will not use `<CSSReset />
+                 */
+                resetCSS: true,
+                /**
+                 * @property {boolean} [isUsingColorMode=true]
+                 * if false, this plugin will not use <ColorModeProvider />
+                 */
+                isUsingColorMode: true,
+            },
+        },
+    ],
 };
 
 export default config;
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,37 +1,34 @@
 exports.createPages = ({ actions, graphql }) => {
-    const { createPage } = actions
-  
-    const blogPostTemplate = require.resolve(`./src/templates/page-template.tsx`)
-  
+    const { createPage } = actions;
+
+    const blogPostTemplate = require.resolve(`./src/templates/page-template.tsx`);
+
     return graphql(`
-      {
-        allMarkdownRemark(
-          limit: 1000
-        ) {
-          edges {
-            node {
-              frontmatter {
-                slug
-              }
+        {
+            allMarkdownRemark(limit: 1000) {
+                edges {
+                    node {
+                        frontmatter {
+                            slug
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    `).then(result => {
-      if (result.errors) {
-        return Promise.reject(result.errors)
-      }
-  
-      return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-          path: node.frontmatter.slug,
-          component: blogPostTemplate,
-          context: {
-            // additional data can be passed via context
-            slug: node.frontmatter.slug,
-          },
-        })
-      })
-    })
-  }
-  
+    `).then((result) => {
+        if (result.errors) {
+            return Promise.reject(result.errors);
+        }
+
+        return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+                path: node.frontmatter.slug,
+                component: blogPostTemplate,
+                context: {
+                    // additional data can be passed via context
+                    slug: node.frontmatter.slug,
+                },
+            });
+        });
+    });
+};
